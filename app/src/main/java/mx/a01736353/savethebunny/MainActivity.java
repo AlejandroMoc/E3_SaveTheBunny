08ab6040c2ea48599247666ae10708fd7486d815
Alejandro M. <a01736353@tec.mx>
2023-10-16 11:24:20 -0600

    Creado proyecto de videojuego

---
+++ app/src/main/java/mx/a01736353/savethebunny/MainActivity.java
@@ -0,0 +1,20 @@
+package mx.a01736353.savethebunny;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view) {
+        GameView gameView = new GameView(this);
+        setContentView(gameView);
+    }
+}
